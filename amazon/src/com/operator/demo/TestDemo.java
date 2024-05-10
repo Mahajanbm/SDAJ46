a3ca8291ef821cd64fa4f3e619922e698473b2d2
Admin <Admin@DESKTOP-476JOGT>
2024-05-10 11:23:57 +0530

    comit

---
+++ amazon/src/com/operator/demo/TestDemo.java
@@ -0,0 +1,13 @@
+package com.operator.demo;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+public class TestDemo {
+	public static void main(String[] args) {
+	Collection c1=new ArrayList<>();
+		
+		
+		
+	}
+}
